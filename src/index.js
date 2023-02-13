d3132e146d9abe5ce6475b8f5b096b81191a46aa
Gabrielldev0 <gabrielldev0@gmail.com>
2023-02-13 20:38:41 -0300

    ContactController

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
-
-console.log('Teste');
-
-app.get('/', (request, response) => {
-  response.send('Hello World');
-});
+app.use(routes);
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
