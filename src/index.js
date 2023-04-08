d407ee3036881d0b9de18b7e2e5e938590ae7211
Gabrielldev0 <gabrielldev0@gmail.com>
2023-04-08 12:53:26 -0300

    Cors and Clean Code Middlewares

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ const express = require('express');
 
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
+
 const routes = require('./routes');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors);
 app.use(routes);
 // Error Handler -> Sempre depois da definição de Rotas.
-app.use((error, request, response, next) => {
-  console.log('#### Error Handler');
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
-app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
+app.listen(3001, () => console.log('🔥 Server started at http://localhost:3001'));
